fb7765424904592f0ad125faf247f19da3b3e272
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-02-13 19:31:08 -0700

    fix repeated notes bug and change colors

--- src/dom.js
+++ src/dom.js
@@ -10,23 +10,22 @@ export const createButton = (onClickHandler) => {
 }
 
 
-export const updateNotes = (event) => {
+export const selectProject = (event) => {
     const clickedProjectId = parseInt(event.target.getAttribute('data-id'));
-    const noteDisplay = document.querySelector('div.notes');
-    const createNote = createButton(showDialogNote)
-
-    noteDisplay.textContent = '';
-    noteDisplay.style.backgroundColor = "#D4D2D5";
-    createNote.textContent = 'Create a new note'
-    createNote.classList.add('new-note-button');
     projectSelection = clickedProjectId;
-
-    noteDisplay.appendChild(createNote);
     updateSidebar();
     displayNotes();
 }
 
-const displayNotes = () => {
+export const displayNotes = () => {
+    const noteDisplay = document.querySelector('div.notes');
+    const buttonNewNote = createButton(showDialogNote)
+
+    noteDisplay.textContent = '';
+    noteDisplay.style.backgroundColor = "#D4D2D5";
+    buttonNewNote.textContent = 'Create a new note'
+    noteDisplay.appendChild(buttonNewNote);
+    buttonNewNote.classList.add('new-note-button');
     for (let element of projects[projectSelection].getList()) {
         createNote(element);
     }
@@ -53,14 +52,14 @@ export const updateSidebar = () => {
     const projectsContainer = document.querySelector('div.project-container');
     projectsContainer.textContent = '';
     for (let [index, item] of projects.entries()) {
-        const button = createButton(updateNotes);
+        const button = createButton(selectProject);
         button.classList.add('project');
         button.setAttribute(`data-id`, index)
         button.textContent = item.title;
         projectsContainer.appendChild(button);
     }
     const currentlySelected = document.querySelector(`[data-id="${projectSelection}"]`)
-    currentlySelected.style.backgroundColor = "#3ABEFF";
+    currentlySelected.classList.add('selected-project');
 }
 
 
@@ -74,22 +73,7 @@ const showDialogNote = () => {
     cancelDialog.addEventListener('click', closeDialog);
 }
 
-const showDialogProject = () => {
-    const dialog = document.querySelector(`dialog.create-project`);
-    dialog.showModal();
-    const acceptDialog = document.querySelector(`form.create-project`);
-    console.table(acceptDialog.elements)
-    acceptDialog.addEventListener('submit', confirmProject);
-    const cancelDialog = document.querySelector('button.cancel');
-    cancelDialog.addEventListener('click', closeDialog);
-}
-
-const createProject = (function() {
-    const createProjectButton = document.querySelector('.button-project');
-    createProjectButton.addEventListener('click', showDialogProject);
-})()
-
-const closeDialog = (event) => {
+export const closeDialog = (event) => {
     const dialog = document.querySelector('dialog');
     event.preventDefault();
     dialog.close()
@@ -109,7 +93,7 @@ const confirmNote = (event) => {
     displayNotes();
 }
 
-const confirmProject = (event) => {
+export const confirmProject = (event) => {
     const dialog = document.querySelector('dialog.create-project');
     event.preventDefault();
     const formData = new FormData(event.target);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import "./styles.css"
-import {updateSidebar} from './dom.js'
+import {updateSidebar, confirmProject, closeDialog, displayNotes} from './dom.js'
 import {projects, Project} from './data.js'
 
 const defaultSidebar = (function(){
     let newProject = new Project("A sample project");
     projects.push(newProject);
     updateSidebar();
+    displayNotes();
 })();
+
+
+
+const showDialogProject = () => {
+    const dialog = document.querySelector(`dialog.create-project`);
+    dialog.showModal();
+    const acceptDialog = document.querySelector(`form.create-project`);
+    console.table(acceptDialog.elements)
+    acceptDialog.addEventListener('submit', confirmProject);
+    const cancelDialog = document.querySelector('button.cancel');
+    cancelDialog.addEventListener('click', closeDialog);
+}
+
+const createProject = (function() {
+    const createProjectButton = document.querySelector('.button-project');
+    createProjectButton.addEventListener('click', showDialogProject);
+})()
